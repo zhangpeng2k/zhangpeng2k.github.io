850c3f91748955c94af92f7fb3cdb88deb7a1a25
zhangpeng2k <1028158677@qq.com>
2020-04-27 14:37:38 +0800

    hello world

--- commit.sh
+++ commit.sh
@@ -14,7 +14,7 @@ git commit -m "$TEXT"
 
 git pull origin hexo
 
-git push origin master
+git push origin hexo
 
 # curl https://sc.ftqq.com/SCU10625Td571049c53dd2e36148f1343a18544ef59855df9df77c.send?text="$TEXT"
 
